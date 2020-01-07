da24f76649f2cfa51618643fe56a877ec7f489f5
DaniWestSide <52814364+DaniWS@users.noreply.github.com>
2020-01-07 03:34:53 +0100

    Add files via upload

---
+++ rest/rest/src/main/java/afc/rest/Main.java
@@ -0,0 +1,114 @@
+package afc.rest;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpContainer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+//import org.glassfish.jersey.servlet.ServletContainer;
+import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
+
+
+
+import org.glassfish.grizzly.http.server.ServerConfiguration;
+import org.glassfish.grizzly.servlet.ServletRegistration;
+import org.glassfish.grizzly.servlet.WebappContext;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.grizzly2.*;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.grizzly.servlet.*;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
+import io.swagger.jaxrs.config.BeanConfig;
+import java.io.IOException;
+import java.net.URI;
+
+
+/**
+ * Main class.
+ *
+ */
+public class Main {
+    // Base URI the Grizzly HTTP server will listen on
+    public static final String BASE_URI = "http://0.0.0.0:8080/";
+    private static final String JERSEY_SERVLET_CONTEXT_PATH = "";
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     * @return Grizzly HTTP server.
+     */
+    public static HttpServer getServerLookup() {
+
+    	String resources = "afc.rest";
+
+    	BeanConfig beanConfig = new BeanConfig();
+
+    	beanConfig.setVersion("1.0.1");
+
+    	beanConfig.setSchemes(new String[] { "http" });
+
+    	beanConfig.setBasePath("");
+
+    	beanConfig.setResourcePackage(resources);
+
+    	beanConfig.setScan(true);
+
+    	final ResourceConfig resourceConfig = new ResourceConfig();
+
+    	resourceConfig.packages(resources);
+
+    	resourceConfig.register(io.swagger.jaxrs.listing.ApiListingResource.class);
+
+    	resourceConfig.register(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+
+    	resourceConfig.register(JacksonFeature.class);
+
+    	resourceConfig.register(JacksonJsonProvider.class);
+    	 
+    	return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), resourceConfig);
+
+    	}
+   /* 
+    public static HttpServer startServer() {
+        // create a resource config that scans for JAX-RS resources and providers
+        // in afc.rest package
+        final ResourceConfig rc = new ResourceConfig().packages("afc.rest");
+
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+*/
+    /**
+     * Main method.
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = getServerLookup();
+        server.start();
+        ClassLoader loader = Main.class.getClassLoader();
+
+        CLStaticHttpHandler docsHandler = new CLStaticHttpHandler(loader, "swagger-ui/");
+        
+
+        docsHandler.setFileCacheEnabled(false);
+
+        ServerConfiguration cfg = server.getServerConfiguration();
+
+        cfg.addHttpHandler(docsHandler, "/docs/");
+        
+/*
+        WebappContext context = new WebappContext("WebappContext", JERSEY_SERVLET_CONTEXT_PATH);
+        ServletRegistration registration = context.addServlet("ServletContainer", ServletContainer.class);
+        registration.setInitParameter(ServletContainer., 
+                ResourceConfig.class.getName());
+        registration.setInitParameter(ClassNamesResourceConfig.PROPERTY_CLASSNAMES, LolCat.class.getName());
+        registration.addMapping("/*");
+        context.deploy(httpServer);
+        */        
+        System.out.println(String.format("Jersey app started with WADL available at "
+                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+        System.in.read();
+        server.stop();
+    }
+}
+
---
+++ rest/rest/src/main/java/afc/rest/NotFoundExceptionMapper.java
@@ -0,0 +1,18 @@
+package afc.rest;
+
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+// This class handles the NotFoundExceptions, and returns a response to the client with the API documentation.
+
+@Provider
+public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
+	
+	public Response toResponse(NotFoundException exception) {
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity("The resource doesn't exist. Please refer to the API documentation: "+ Server.docsUri)
+                .build();
+    }
+}
