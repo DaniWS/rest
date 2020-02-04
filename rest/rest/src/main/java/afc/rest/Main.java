c5a826b0b32895b7c8bbcd42e6d63b706b44c514
DaniWS <dani010lenin@gmail.com>
2020-02-04 03:06:00 +0100

    docker_ssl

--- rest/rest/src/main/java/afc/rest/Main.java
+++ rest/rest/src/main/java/afc/rest/Main.java
@@ -31,7 +31,16 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.stream.Stream;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.X509TrustManager;
 
 
 /**
@@ -40,8 +49,29 @@ import java.util.stream.Stream;
  */
 public class Main {
 	// Base URI the Grizzly HTTP server will listen on
-	public static final String BASE_URI = "https://127.0.0.1:8080/";
-    
+	public static final String BASE_URI = "https://0.0.0.0:8080/";
+	
+	private static void trustEveryone() { 
+	    try { 
+	            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier(){ 
+	                    public boolean verify(String hostname, SSLSession session) { 
+	                            return true; 
+	                    }}); 
+	            SSLContext context = SSLContext.getInstance("TLS"); 
+	            context.init(null, new X509TrustManager[]{new X509TrustManager(){ 
+	                    public void checkClientTrusted(X509Certificate[] chain, 
+	                                    String authType) throws CertificateException {} 
+	                    public void checkServerTrusted(X509Certificate[] chain, 
+	                                    String authType) throws CertificateException {} 
+	                    public X509Certificate[] getAcceptedIssuers() { 
+	                            return new X509Certificate[0]; 
+	                    }}}, new SecureRandom()); 
+	            HttpsURLConnection.setDefaultSSLSocketFactory( 
+	                            context.getSocketFactory()); 
+	    } catch (Exception e) { // should never happen 
+	            e.printStackTrace(); 
+	    } 
+	} 
    
     
     /**
@@ -112,6 +142,11 @@ public class Main {
         cfg.addHttpHandler(docsHandler, "/docs/");
         cfg.addHttpHandler(schemasHandler, "/schemas/");
 
+
+
+
+
+        trustEveryone();
         SchemaLoader.loadSchemas(BASE_URI+"schemas/");
         
      
